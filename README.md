aa88338ee5f94ed69d2956b928c7b2847b8735ca
haipengliu <pliu1981@gmail.com>
2014-10-19 16:56:14 -0700

    2014-10-20

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ Flask
 =====
 
 Flask Web Infrastructure using python
+
+=Setup Enviroment=
+Need:
+virtualenv
+pip
+python-dev
+
+=INSTALL=
+sudo apt-get install python-virtualenv
+virtualenv venv
+. venv/bin/activate
+pip install Flask (or pip install Flask==dev)
+
+
+
+
+
+
--- tutorial/flaskr/flaskr.py
+++ tutorial/flaskr/flaskr.py
@@ -1,29 +1,108 @@
-# application moduel, by haipeng #
 
-# all the imports
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import sqlite3, os
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
 
-# DATABASE's import & configuration
-# Can be putted here or create a indepedent .ini or .py file, then import here
-DATABASE   = 'tmp/flaskr.db'
-DEBUG      = True
-SECRET_KEY = 'development key'
-USERNAME   = 'admin'
-PASSWORD   = 'default'
 
-# create out little application, config it with the same file
 app = Flask(__name__)
+
+# configuration
 app.config.from_object(__name__)
 
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
 def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
-if __name__=='__main__':
+'''You must run from flaskr import init_db; init_db(); exit(); to produce the flaskr.db file'''
+def init_db():
+     with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+        return db
+
+def get_db():
+    """Opens a new database connection if there is none yet for the current application context."""
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = init_db()
+    else:
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.before_request
+def before_request():
+    g.sqlite_db = connect_db()
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('show_entries'))
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    init_db()
+    g.sqlite_db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.sqlite_db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+if __name__ == '__main__':
     app.debug = True
-    app.run(host='172.27.17.9:5000')
-
-
-
-
-
+    app.run(host='192.168.30.129')
